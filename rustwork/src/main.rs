afaf0eaeabef5204c0a152f481024ef5a9bf496a
Haardik <ravat.1@iitj.ac.in>
2022-06-11 21:29:52 +0530

    functions

--- rustwork/src/main.rs
+++ rustwork/src/main.rs
@@ -1,11 +1,21 @@
 fn main() {
 
-    let arr:[u8;3]=[1,2,3];
-    let arr:[u8;3]=[1;3];
-    let l=arr.len();
-    // let a=10;
-    // let b=15;
-    // let letter="c";
-    // let emoji="\u{1F600}";
-    println!("Hello, world!,{}{}",arr[0],l);
+    // let arr:[u8;3]=[1,2,3];
+    // let arr:[u8;3]=[1;3];
+    // let l=arr.len();
+    // // let a=10;
+    // // let b=15;
+    // // let letter="c";
+    // // let emoji="\u{1F600}";
+    // println!("Hello, world!,{}{}",arr[0],l);
+
+
+println!("{}", eve(2));
+}
+
+pub fn eve(num:u8)-> bool{
+    let digit: u8=num%2;
+    digit==0 // return- no semicolon
+
+
 }
